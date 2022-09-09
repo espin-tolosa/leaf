7557112a18de5b2546086667f9a0c555a292f7a4
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-09-09 03:47:56 +0200

    Refactor: Separation of Concerns, create a framework class to handle request

--- src/public/index.php
+++ src/public/index.php
@@ -2,66 +2,81 @@
 
 require_once realpath(__DIR__ . '/../../vendor/autoload.php');
 
-use Set\Framework\App\Http\Response as HttpResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing;
-
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 /**
- * Import Routes
+ * Request / Response process
+ *
+ * 1.			Match Request
+ ! 2.			Call Controller (busines logic and rewrite the status, only here)
+ * 2.1.			Get Request Parameters, Busines Logic, Rewrite Status Code if needed and Call Renderer with Proccessed parameters
+ * 2.2.  		Create a Response reading some new status that could be possibly generated in Controller
+ ! 2.3.			Call View Template OB Renderer
+ * 2.4.   	Return Response with content, status, header
+ * 3.			Send Response to the client
+ */
+class Framework {
+	
+	private UrlMatcher $matcher;
+	private ControllerResolver $controllerResolver;
+	//private ArgumentResolver $argumentResolver;
+	
+	public function __construct(UrlMatcher $matcher, ControllerResolver $controllerResolver /*, ArgumentResolver $argumentResolver */ ) {
+		$this->matcher = $matcher;
+		$this->controllerResolver = $controllerResolver;
+		//$this->argumentResolver = $argumentResolver;
+	}
+	
+	public function handle(Request $request) {
+		$this->matcher->getContext()->fromRequest($request);
+	
+		try {
+			$request->attributes->add($this->matcher->match($request->getPathInfo()));
+			
+			$controller = $this->controllerResolver->getController($request);
+			//$arguments = $this->argumentResolver->getArguments($request, $controller);
+
+			return call_user_func($controller, $request);
+		}
+		
+		catch (Routing\Exception\ResourceNotFoundException $exception) {
+			$request->attributes->add($this->matcher->match('/not-found'));
+			$request->attributes->add(['exception' => $exception->getMessage()]);
+			
+			$controller = $this->controllerResolver->getController($request);
+			return call_user_func($controller, $request);
+		}
+		
+		catch (Exception $exception) {
+			$request->attributes->add($this->matcher->match('/server-error'));
+			$request->attributes->add(['exception' => $exception->getMessage()]);
+		
+			$controller = $this->controllerResolver->getController($request);
+			return call_user_func($controller, $request);
+		}
+	}
+	
+	
+}
+
+/**
+ * Read Routes
  */
 
 require_once realpath(__DIR__ . '/../app/routes/web.php');
-
-/**
- * Read Request
- */
- 
 $request = Request::createFromGlobals();
-$context = new Routing\RequestContext();
-$context->fromRequest($request);
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-/**
- * Template rendering
- *
- * TODO: Move (2.2) Response Creation to (2.1.3) Controller, which is capable to set headers, so no needs to pass status through request object
- *
- * 1.			Match Request
- ! 2.    	Call Send View Wrapper (just sugar DRY for try/catch)
- ! 2.1.			Call Controller (busines logic and rewrite the status, only here)
- * 2.1.1.			Get Request Parameters, Busines Logic, Rewrite Status Code if needed and Call Renderer with Proccessed parameters
- ! 2.1.2.			Call View Template OB Renderer
- * 2.2.   	Create and Return a Response reading some new status that could be possibly generated in Controller
- * 2.3.   	Send Response
- */
-	
-try {
-	$request->attributes->add($matcher->match($request->getPathInfo()));
+$context = new RequestContext();
+$matcher = new UrlMatcher($routes, $context);
 
-	$controllerResolver = new ControllerResolver();
-	$controller = $controllerResolver->getController($request);
-	$response = call_user_func($controller, $request);
-}
+$controllerResolver = new ControllerResolver();
+//$argumentResolver = new ArgumentResolver();
 
-catch (Routing\Exception\ResourceNotFoundException $exception) {
-	$request->attributes->add($matcher->match('/not-found'));
-	$request->attributes->add(['exception' => $exception->getMessage()]);
-
-	$controllerResolver = new ControllerResolver();
-	$controller = $controllerResolver->getController($request);
-	$response = call_user_func($controller, $request);
-}
-
-catch (Exception $exception) {
-	$request->attributes->add($matcher->match('/server-error'));
-	$request->attributes->add(['exception' => $exception->getMessage()]);
-
-	$controllerResolver = new ControllerResolver();
-	$controller = $controllerResolver->getController($request);
-	$response = call_user_func($controller, $request);
-}
-
+$framework = new Framework($matcher, $controllerResolver /*, $argumentResolver*/);
+$response = $framework->handle($request);
 $response->send();
