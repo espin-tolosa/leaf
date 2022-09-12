1952905c934be1e6d45c8e77645fdc5847dcac84
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-09-12 18:30:45 +0200

    Add: testing respond with a JS resource

--- tests/UseCases/curl/get_index.sh
+++ tests/UseCases/curl/get_index.sh
@@ -17,3 +17,7 @@ assert_eq "$expected" "$response" "Unauthorized request"
 response=$(curl -s --location --request GET 'http://localhost:4321/user/sam' --header 'Cookie: jwt=sam' | grep -o 'Hello sam');
 expected='Hello sam';
 assert_eq "$expected" "$response" "Authorized request"
+
+response=$(curl -s 'http://localhost:4321/public/spa.master.b26f2810.js' -H \
+'Cookie: jwt=sam' | grep -o "Xa=function")
+assert_eq "$response" "Xa=function" "public JS resource"
